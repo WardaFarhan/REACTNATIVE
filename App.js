426f9a519e4913ba87108b9e23e4ed1e8f45f516
WardaFarhan <farhan.warda337@gmail.com>
2020-07-27 17:27:59 +0530

    Components Part 1

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
+import React from 'react';
+import Main from './components/MainComponent';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
+
+/*
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,7 +18,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello WARDA FARHAN!<br/>
+      <Text>Hello WARDA FARHAN!
       Open up App.js to start working on your app!
       </Text>
       <StatusBar style="auto" />
@@ -21,3 +34,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
